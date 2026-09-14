e3a027887f75d77541f0fa07b287c3d113afde90
Nat Sriprang <nat.sriprang@student.unimelb.edu.au>
2026-09-14 06:09:26 +0000

    update codes ai

--- test.py
+++ test.py
@@ -8,18 +8,21 @@ st.title("Environment creation")
 name = st.text_input("What is your name?")
 if "name" not in st.session_state:
     st.session_state.name = ""
+
 if st.button("Click"):
     if name == "Nat":
         st.write("Welcome back")
+        client = OpenAI()
+
+        response = client.responses.create(
+            model="gpt-4o",
+            input=f"write a greetings to {name}."       
+)
+        st.write(response.output_text)
+    
     else:
         st.write("You cannot come in here!")
 
-client = OpenAI()
 
-response = client.responses.create(
-    model="gpt-4o",
-    input=f"write a greetings to {name}."       
-)
-st.write(response.output_text)
 
 
